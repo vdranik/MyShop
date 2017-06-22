4824950cf394812ef545e23a81c0f704d35ba248
Vladimir Dranik <v.dranik@gmail.com>
2017-06-22 20:12:04 +0300

    add_loop_for_productList

--- src/main/java/com/myshop/dao/ProductDao.java
+++ src/main/java/com/myshop/dao/ProductDao.java
@@ -13,19 +13,40 @@ public class ProductDao {
     private List<Product> productList;
 
     public List<Product> getProductList(){
-        Product product_example = new Product();
+        Product product_example_1 = new Product();
+        product_example_1.setProductName("XBOX ONE");
+        product_example_1.setProductCategory("Console");
+        product_example_1.setProductDescription("Perfect gamestation");
+        product_example_1.setProductPrice(500);
+        product_example_1.setProductCondition("NEW");
+        product_example_1.setProductStatus("ACTIVE");
+        product_example_1.setUnitInStock(11);
+        product_example_1.setProductManufacturer("Microsoft");
 
-        product_example.setProductName("XBOX ONE");
-        product_example.setProductCategory("Game Station");
-        product_example.setProductDescription("Perfect gamestation");
-        product_example.setProductPrice(500);
-        product_example.setProductCondition("NEW");
-        product_example.setProductStatus("ACTIVE");
-        product_example.setUnitInStock(11);
-        product_example.setProductManufacturer("Microsoft");
+        Product product_example_2 = new Product();
+        product_example_2.setProductName("Far Cry 5");
+        product_example_2.setProductCategory("Game");
+        product_example_2.setProductDescription("SupaPupa Game");
+        product_example_2.setProductPrice(40);
+        product_example_2.setProductCondition("NEW");
+        product_example_2.setProductStatus("ACTIVE");
+        product_example_2.setUnitInStock(111);
+        product_example_2.setProductManufacturer("Ubisoft");
+
+        Product product_example_3 = new Product();
+        product_example_3.setProductName("Far Cry 5 poster");
+        product_example_3.setProductCategory("Accessory");
+        product_example_3.setProductDescription("SupaPupa Poster");
+        product_example_3.setProductPrice(4);
+        product_example_3.setProductCondition("NEW");
+        product_example_3.setProductStatus("ACTIVE");
+        product_example_3.setUnitInStock(1111);
+        product_example_3.setProductManufacturer("ChinaPrintLTD");
 
         productList = new ArrayList<Product>();
-        productList.add(product_example);
+        productList.add(product_example_1);
+        productList.add(product_example_2);
+        productList.add(product_example_3);
         return productList;
     }
 
--- src/main/webapp/WEB-INF/views/productList.jsp
+++ src/main/webapp/WEB-INF/views/productList.jsp
@@ -87,13 +87,16 @@
                     <th>Price</th>
                 </tr>
             </thead>
-            <tr>
-                <td><img src="#" alt="image"></td>
-                <td>${product.productName}</td>
-                <td>${product.productCategory}</td>
-                <td>${product.productCondition}</td>
-                <td>${product.productPrice}</td>
-            </tr>
+
+            <c:forEach items="${products}" var="product">
+                <tr>
+                    <td><img src="#" alt="image"></td>
+                    <td>${product.productName}</td>
+                    <td>${product.productCategory}</td>
+                    <td>${product.productCondition}</td>
+                    <td>${product.productPrice}</td>
+                </tr>
+            </c:forEach>
         </table>
         <!-- FOOTER -->
         <footer>
