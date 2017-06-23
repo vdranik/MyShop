ee88a24a8f56921d88689ff4de7772aed824a83b
Vladimir Dranik <v.dranik@gmail.com>
2017-06-23 19:09:01 +0300

    implement_ProductDao

--- src/main/java/com/myshop/controller/HomeController.java
+++ src/main/java/com/myshop/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.myshop.controller;
 
 import com.myshop.dao.ProductDao;
 import com.myshop.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,8 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    private ProductDao productDao = new ProductDao();
+    @Autowired
+    private ProductDao productDao;
 
     @RequestMapping("/")
     public String home(){
@@ -25,7 +27,7 @@ public class HomeController {
 
     @RequestMapping("/productList")
     public String getProducts(Model model){
-        List<Product> products = productDao.getProductList();
+        List<Product> products = productDao.getAllProducts();
         model.addAttribute("products", products);
 
         return "productList";
--- src/main/java/com/myshop/model/Product.java
+++ src/main/java/com/myshop/model/Product.java
@@ -1,10 +1,18 @@
 package com.myshop.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by User on 6/22/2017.
  */
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String productId;
     private String productName;
     private String productCategory;
