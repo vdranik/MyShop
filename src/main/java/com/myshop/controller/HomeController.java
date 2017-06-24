0e0a3095f5963427fecc546a60489a89c107abc6
Vladimir Dranik <v.dranik@gmail.com>
2017-06-24 15:58:43 +0300

    add_addProduct_submit

--- src/main/java/com/myshop/controller/HomeController.java
+++ src/main/java/com/myshop/controller/HomeController.java
@@ -5,8 +5,10 @@ import com.myshop.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.IOException;
 import java.util.List;
@@ -63,5 +65,12 @@ public class HomeController {
 
         model.addAttribute("product", product);
         return "addProduct";
+    }
+
+    @RequestMapping(value = "/admin/productInventory/addProduct", method = RequestMethod.POST)
+    public String addProductPost(@ModelAttribute("product") Product product){
+        productDao.addProduct(product);
+
+        return "redirect:/admin/productInventory";
     }
 }
